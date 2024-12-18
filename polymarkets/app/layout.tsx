6a7a6b758b9543a54ba3b1ee74161f4eaa200ac5
Luqxus <luqxus@localhost.localdomain>
2024-12-19 00:36:53 +0200

    claim winnings, home page state management

--- polymarkets/app/layout.tsx
+++ polymarkets/app/layout.tsx
@@ -1,32 +1,30 @@
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import './globals.css'
-import { ThirdwebProvider } from 'thirdweb/react'
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import { ThirdwebProvider } from "thirdweb/react";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Polymarket Clone',
-  description: 'A clone of the Polymarket prediction markets interface',
-}
+  title: "Uzarpoly",
+  description: "Uzarpoly prediction markets",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en" className="dark">
       <ThirdwebProvider>
-        <body className={`${inter.className} bg-[#1a2027] text-white min-h-screen`}>
+        <body
+          className={`${inter.className} bg-[#1a2027] text-white min-h-screen`}
+        >
           {/* <Topbar /> */}
           {children}
         </body>
       </ThirdwebProvider>
     </html>
-  )
+  );
 }
-
-
-
-
